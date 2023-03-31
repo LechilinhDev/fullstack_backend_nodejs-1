ceadc17eff26d01a7f6c8241cc40b7e245be5b01
Le Chi Linh Dev <dothihue2689@gmail.com>
2023-03-31 15:27:33 +0700

    add user in project

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -5,5 +5,17 @@ module.exports = {
             let result = await Project.create(data);
             return result;
         }
+        if (data.type === "ADD-USER") {
+
+            let findProject = await Project.findById(data.projectId).exec();
+            for (let i = 0; i < data.userArr.length; i++) {
+                findProject.usersInfor.push(data.userArr[i]);
+            }
+
+            let newResult = findProject.save();
+            console.log('....check data', findProject);
+            return newResult
+        }
+        return null;
     }
 }
